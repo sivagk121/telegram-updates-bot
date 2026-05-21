b5e92357f1615cf1be5ed8353cb85a93ae12ef80
sivagk121 <sivaganesh9609@gmail.com>
2026-05-21 12:03:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,53 +5,80 @@ BOT_TOKEN = "8686311310:AAHAALy0hOh-2dp98wo4rQFVmcw-taEd7NM"
 CHANNEL_ID = "@sivagk121"
 
 RRB_SITES = {
-"Secunderabad":"https://rrbsecunderabad.gov.in/",
-"Chennai":"https://www.rrbchennai.gov.in/",
-"Bangalore":"https://www.rrbbnc.gov.in/"
+"Chennai":"https://www.rrbchennai.gov.in/"
 }
 
 def send(msg):
     requests.post(
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         data={
-            "chat_id": CHANNEL_ID,
-            "text": msg
+            "chat_id":CHANNEL_ID,
+            "text":msg
         }
     )
 
-for board,url in RRB_SITES.items():
 
-    try:
-        page=requests.get(url,timeout=10)
-        soup=BeautifulSoup(page.text,"html.parser")
+try:
 
-        links=soup.find_all("a")
+    page=requests.get(
+        "https://www.rrbchennai.gov.in/",
+        timeout=20
+    )
 
-        count=0
+    soup=BeautifulSoup(
+        page.text,
+        "html.parser"
+    )
 
-        for l in links:
+    links=soup.find_all("a")
 
-            text=l.get_text(" ",strip=True)
-            href=l.get("href")
+    count=0
 
-            if text and href and len(text)>10:
+    for l in links:
 
-                if not href.startswith("http"):
-                    href=url+href
+        text=l.get_text(
+            " ",
+            strip=True
+        )
 
-                send(
-f"""🚨 {board} Test
+        href=l.get("href")
+
+        if not text or not href:
+            continue
+
+
+        low=text.lower()
+
+        if any(
+            x in low
+            for x in [
+                "result",
+                "notification",
+                "cen",
+                "alp",
+                "ntpc",
+                "je",
+                "answer",
+                "exam"
+            ]
+        ):
+
+            send(
+f"""🚨 Chennai RRB
 
 {text}
 
 🔗 {href}
 """
-                )
+            )
 
-                count +=1
+            count +=1
 
-            if count==5:
-                break
 
-    except Exception as e:
-        send(f"{board} Error:\n{e}")
+        if count==10:
+            break
+
+
+except Exception as e:
+
+    send(str(e))
