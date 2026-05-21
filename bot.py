023beef583155e98724700c587f5b88bb0530e82
sivagk121 <sivaganesh9609@gmail.com>
2026-05-21 08:41:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,19 @@ import requests
 BOT_TOKEN = "8686311310:AAHugfm5p7GBHXK-s_2RefiJQBFlpF-HZts"
 CHANNEL_ID = "@sivagk121"
 
-message = "🚨 GitHub automation test"
+updates = [
+    "🚨 RRB: Check latest notifications",
+    "🚨 SSC: Check latest updates",
+    "🚨 UPSC: New notification updates"
+]
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+for message in updates:
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-data = {
-    "chat_id": CHANNEL_ID,
-    "text": message
-}
+    data = {
+        "chat_id": CHANNEL_ID,
+        "text": message
+    }
 
-requests.post(url, data=data)
+    response = requests.post(url, data=data)
+    print(response.text)
