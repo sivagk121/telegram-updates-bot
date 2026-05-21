b85805c367922d5f5919e770bfd94ae1786cf4e7
sivagk121 <sivaganesh9609@gmail.com>
2026-05-21 10:06:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 import requests
 
-BOT_TOKEN = "8686311310:AAHugfm5p7GBHXK-s_2RefiJQBFlpF-HZts"
+BOT_TOKEN = "8686311310:AAHAALy0hOh-2dp98wo4rQFVmcw-taEd7NM"
 CHANNEL_ID = "@sivagk121"
 
 updates = [
-    "🚨 RRB: Check latest notifications",
-    "🚨 SSC: Check latest updates",
-    "🚨 UPSC: New notification updates"
+    "🚨 RRB: Latest railway updates check",
+    "🚨 SSC: Latest SSC updates check"
 ]
 
 for message in updates:
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
     data = {
         "chat_id": CHANNEL_ID,
         "text": message
     }
+
     requests.post(url, data=data)
 
-print("NEW CODE RUNNING")
+print("RRB + SSC workflow running")
